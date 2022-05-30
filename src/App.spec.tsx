b46b99d54186a140716c43ed4b1b789517d1db00
Alan Cleyton <alancleyton67@gmail.com>
2022-05-30 15:15:12 -0300

    add test setup with jest and testing-library

---
+++ src/App.spec.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe('App', () => {
+  it('should render with "Hello World" title', () => {
+    render(<App />);
+    const title = screen.getByRole('heading', { name: /Hello World!/i });
+    expect(title).toBeInTheDocument();
+  });
+});
