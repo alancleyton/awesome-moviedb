240e862aea11fead33a0622ac5ea1af128dcc2ee
Alan Cleyton <alancleyton67@gmail.com>
2023-09-10 13:11:25 -0300

    style: add app base styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from '@/routes';
+import './App.css';
 
 function App(): JSX.Element {
   return (
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -30,6 +30,7 @@ module.exports = {
       heading: 1.25,
     },
     spacing: {
+      0: '0px',
       1: '2px',
       2: '4px',
       3: '8px',
