0749a0705c05a05a85d3e556808ffdca982fbaa6
Alan Cleyton <alancleyton67@gmail.com>
2023-11-19 10:39:56 -0300

    fix: adjust header navbar search on mobile

--- src/components/header/header-navbar-search.tsx
+++ src/components/header/header-navbar-search.tsx
@@ -1,34 +1,102 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import * as MdIcon from 'react-icons/md';
 
-import { Input } from '@/components';
+import { withResize } from '@/hoc/withResize';
+import { cn } from '@/utils/cn';
+import { Input, Button } from '@/components';
 import { HeaderNavbarSearchSuggestions } from './header-navbar-search-suggestions';
 
-export const HeaderNavbarSearch = () => {
+interface HeaderNavbarSearchProps {
+  // eslint-disable-next-line
+  windowSize?: number;
+  serchOnMobile: boolean;
+  onToggleSerchOnMobile: (param: boolean) => void;
+}
+
+const HeaderNavbarSearch = ({
+  windowSize,
+  serchOnMobile,
+  onToggleSerchOnMobile,
+}: HeaderNavbarSearchProps) => {
   const [searchValue, setSearchValue] = useState<string>('');
   const [isFocused, setIsFocused] = useState<boolean>(false);
-
   const showSearchSuggestions = searchValue && isFocused;
 
   return (
-    <form action="submit" className="relative w-full">
-      <Input.Group>
-        <Input.Icon alignment="left">
-          <MdIcon.MdSearch className="text-gray900" size={24} />
-        </Input.Icon>
+    <form
+      className={cn(
+        'w-full',
+        windowSize &&
+          windowSize <= 768 &&
+          serchOnMobile &&
+          'absolute top-5 left-0',
+      )}
+    >
+      {windowSize && windowSize <= 768 && (
+        <Button
+          type="button"
+          className={serchOnMobile ? 'hidden' : 'ml-auto'}
+          variant="secondary"
+          size="sm"
+          onClick={() => onToggleSerchOnMobile(true)}
+        >
+          <Button.Icon>
+            <MdIcon.MdSearch size={24} />
+          </Button.Icon>
+        </Button>
+      )}
 
-        <Input
-          className="pl-7"
-          type="text"
-          placeholder="Busque filmes, séries de TV, celebridades, e mais..."
-          value={searchValue}
-          onChange={event => setSearchValue(event.target.value)}
-          onFocus={() => setIsFocused(true)}
-          onBlur={() => setIsFocused(false)}
-        />
-      </Input.Group>
+      <div
+        className={
+          (windowSize && windowSize >= 768) || serchOnMobile
+            ? 'relative'
+            : 'hidden'
+        }
+      >
+        <Input.Group>
+          {!serchOnMobile && (
+            <Input.Icon
+              alignment="left"
+              onClick={() => onToggleSerchOnMobile(false)}
+            >
+              <MdIcon.MdSearch className="text-gray900" size={24} />
+            </Input.Icon>
+          )}
 
-      {showSearchSuggestions && <HeaderNavbarSearchSuggestions />}
+          {windowSize && windowSize <= 768 && serchOnMobile && (
+            <Input.Icon alignment="right" className="cursor-pointer">
+              <Button
+                variant="secondary"
+                size="sm"
+                rounded
+                onClick={() => onToggleSerchOnMobile(false)}
+              >
+                <Button.Icon>
+                  <MdIcon.MdClose size={24} />
+                </Button.Icon>
+              </Button>
+            </Input.Icon>
+          )}
+
+          <Input
+            className={
+              windowSize && windowSize <= 768 && serchOnMobile
+                ? 'border-none bg-dark text-gray900'
+                : 'pl-7'
+            }
+            type="text"
+            placeholder="Busque filmes, séries de TV e celebridades"
+            value={searchValue}
+            onChange={event => setSearchValue(event.target.value)}
+            onFocus={() => setIsFocused(true)}
+            onBlur={() => setIsFocused(false)}
+          />
+        </Input.Group>
+
+        {showSearchSuggestions && <HeaderNavbarSearchSuggestions />}
+      </div>
     </form>
   );
 };
+
+export default withResize(HeaderNavbarSearch);
--- src/components/header/header-navbar.tsx
+++ src/components/header/header-navbar.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import * as MdIcon from 'react-icons/md';
 
@@ -6,55 +7,64 @@ import { Container, Menu, Button } from '@/components';
 
 import { HeaderNavbarProps } from './header-types';
 
-import { HeaderNavbarSearch } from './header-navbar-search';
+import HeaderNavbarSearch from './header-navbar-search';
 
-export const HeaderNavbar = ({ onOpen }: HeaderNavbarProps) => (
-  <nav className="bg-dark" role="navigation">
-    <Container>
-      <Menu>
-        <Menu.Item alignment="left" collapse>
-          <Link to="/">
-            <figure className="h-7 w-11 flex items-center justify-center rounded bg-darkSlateBlue">
-              <img src={appImages.LOGO_BRAND_MD} alt="logotipo" />
-            </figure>
-          </Link>
-        </Menu.Item>
+export const HeaderNavbar = ({ onOpen }: HeaderNavbarProps) => {
+  const [onMobileSearch, setOnMobileSearch] = useState<boolean>(false);
 
-        <Menu.Item alignment="left" filled>
-          <HeaderNavbarSearch />
-        </Menu.Item>
+  return (
+    <nav className="bg-dark relative" role="navigation">
+      <Container>
+        <Menu>
+          {!onMobileSearch && (
+            <Menu.Item alignment="left">
+              <Link to="/">
+                <figure className="h-7 w-11 flex items-center justify-center rounded bg-darkSlateBlue">
+                  <img src={appImages.LOGO_BRAND_MD} alt="logotipo" />
+                </figure>
+              </Link>
+            </Menu.Item>
+          )}
 
-        <Menu.Item alignment="right" collapse>
-          <div className="flex items-center gap-2">
-            <Button variant="secondary" size="sm">
-              <Link to="/movies">Filmes</Link>
+          <Menu.Item alignment="left" filled>
+            <HeaderNavbarSearch
+              serchOnMobile={onMobileSearch}
+              onToggleSerchOnMobile={setOnMobileSearch}
+            />
+          </Menu.Item>
+
+          <Menu.Item alignment="right" collapse>
+            <div className="flex items-center gap-2">
+              <Button variant="secondary" size="sm">
+                <Link to="/movies">Filmes</Link>
+              </Button>
+
+              <Button variant="secondary" size="sm">
+                <Link to="/shows">Programas de TV</Link>
+              </Button>
+
+              <Button variant="secondary" size="sm">
+                <Link to="/people">Celebridades</Link>
+              </Button>
+            </div>
+          </Menu.Item>
+
+          <Menu.Item collapse>
+            <span className="h-full w-hr border border-solid border-dark600">
+              &nbsp;
+            </span>
+          </Menu.Item>
+
+          <Menu.Item>
+            <Button variant="secondary" size="sm" onClick={onOpen}>
+              <Button.Icon>
+                <MdIcon.MdMenu size={24} />
+              </Button.Icon>
+              <span>Menu</span>
             </Button>
-
-            <Button variant="secondary" size="sm">
-              <Link to="/shows">Programas de TV</Link>
-            </Button>
-
-            <Button variant="secondary" size="sm">
-              <Link to="/people">Celebridades</Link>
-            </Button>
-          </div>
-        </Menu.Item>
-
-        <Menu.Item collapse>
-          <span className="h-full w-hr border border-solid border-dark600">
-            &nbsp;
-          </span>
-        </Menu.Item>
-
-        <Menu.Item>
-          <Button variant="secondary" size="sm" onClick={onOpen}>
-            <Button.Icon>
-              <MdIcon.MdMenu size={24} />
-            </Button.Icon>
-            <span className="tablet:block hidden">Menu</span>
-          </Button>
-        </Menu.Item>
-      </Menu>
-    </Container>
-  </nav>
-);
+          </Menu.Item>
+        </Menu>
+      </Container>
+    </nav>
+  );
+};
